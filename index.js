bd1d84f7f9ddb7b37508dc374a8cd1c6dbffb2c4
gabimazetto <gmazettog@gmail.com>
2023-04-17 16:52:29 -0300

    Relacionamenteo Cliente-Endereço

--- index.js
+++ index.js
@@ -11,7 +11,31 @@ const { connection, authenticate } = require("./database/database");
 authenticate(connection); // efetivar a conexão 
 const Cliente = require("./database/cliente"); // Configurar o model da aplicação
 const Endereco = require("./database/endereco");
+
+
 // Definição de rotas
+app.post("/clientes", async (req, res) => {
+    // - coletar informações do req.body
+    const { nome, email, telefone, endereco } = req.body;
+
+    try {
+        // Dentro de 'novo' estará o objeto criado
+        const novo = await Cliente.create(
+            { nome, email, telefone, endereco }, 
+            { include: [Endereco]}// Permite insereir cliente e endereço num comando 
+        );
+        res.status(201).json(novo);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ message: "Um erro aconteceu!" });
+    }
+});
+
+// Passos para adicionar:
+// - coletar informações do req.body
+// Chamar o Model e a função create
+
+
 
 //Escuta deventos (listen)
 app.listen(3000, () => {
